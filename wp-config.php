32386b55b9216b28d026c3ba9f9055516e868b46
Pablo ST <pablost88@gmail.com>
2022-09-22 16:52:31 -0300

    display errors

--- wp-config.php
+++ wp-config.php
@@ -79,7 +79,9 @@ $table_prefix = 'ch_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
