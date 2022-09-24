5c01a640edc11fa41836c45bb8b8c94aaeba7b80
Pablo ST <pablost88@gmail.com>
2022-09-24 15:04:34 -0300

    add bidirectional relationship

--- wp-content/plugins/jobsity-challenge/class-jobsity.php
+++ wp-content/plugins/jobsity-challenge/class-jobsity.php
@@ -1,7 +1,7 @@
 <?php
 
 class Jobsity {
-	const TRANSIENT_EXPIRATION = 3600;
+	const TRANSIENT_EXPIRATION = 60 * 60 * 168; // 7 days until the transient gets updated.
 	const API_URL              = 'https://api.themoviedb.org/3/';
 	const API_KEY              = 'd6aee8db7cd6a522157abaf6c8fa7491';
 
@@ -42,6 +42,7 @@ class Jobsity {
 		add_action( 'manage_actor_posts_custom_column', array( 'Jobsity', 'actor_admin_columns_content' ), 10, 2 );
 		add_filter( 'manage_edit-actor_sortable_columns', array( 'Jobsity', 'actor_admin_sortable_columns' ) );
 		add_action( 'pre_get_posts', array( 'Jobsity', 'admin_custom_orders' ) );
+		add_filter( 'acf/update_value/name=actor_movies', array( 'Jobsity', 'bidirectional_acf_update_value' ), 10, 3 );
 	}
 
 
@@ -190,6 +191,7 @@ class Jobsity {
 			'query_var'          => true,
 			'capability_type'    => 'post',
 			'has_archive'        => true,
+			'rewrite'            => array( 'slug' => 'movies' ),
 			'hierarchical'       => false,
 			'taxonomies'         => array( 'genre' ),
 			'supports'           => array( 'title', 'thumbnail', 'editor' ),
@@ -227,6 +229,7 @@ class Jobsity {
 			'query_var'          => true,
 			'capability_type'    => 'post',
 			'has_archive'        => true,
+			'rewrite'            => array( 'slug' => 'actors' ),
 			'hierarchical'       => false,
 			'supports'           => array( 'title', 'thumbnail', 'editor' ),
 		);
@@ -270,6 +273,90 @@ class Jobsity {
 
 
 	/**
+	 * Creates a bidirectional relationship between actors and movies.
+	 */
+	function bidirectional_acf_update_value( $value, $post_id, $field  ) {
+
+		// vars
+		$field_name = $field['name'];
+		$field_key = $field['key'];
+		$global_name = 'is_updating_' . $field_name;
+
+		// bail early if this filter was triggered from the update_field() function called within the loop below
+		// - this prevents an inifinte loop
+		if( !empty($GLOBALS[ $global_name ]) ) return $value;
+
+		// set global variable to avoid inifite loop
+		// - could also remove_filter() then add_filter() again, but this is simpler
+		$GLOBALS[ $global_name ] = 1;	
+
+		// loop over selected posts and add this $post_id
+		if( is_array($value) ) {
+
+			foreach( $value as $post_id2 ) {
+
+				// load existing related posts
+				$value2 = get_field($field_name, $post_id2, false);
+
+				// allow for selected posts to not contain a value
+				if( empty($value2) ) {
+
+					$value2 = array();
+
+				}
+
+				// bail early if the current $post_id is already found in selected post's $value2
+				if( in_array($post_id, $value2) ) continue;
+
+				// append the current $post_id to the selected post's 'related_posts' value
+				$value2[] = $post_id;
+
+				// update the selected post's value (use field's key for performance)
+				update_field($field_key, $value2, $post_id2);
+
+			}
+
+		}
+
+		// find posts which have been removed
+		$old_value = get_field($field_name, $post_id, false);
+
+		if( is_array($old_value) ) {
+
+			foreach( $old_value as $post_id2 ) {
+
+				// bail early if this value has not been removed
+				if( is_array($value) && in_array($post_id2, $value) ) continue;
+
+				// load existing related posts
+				$value2 = get_field($field_name, $post_id2, false);
+
+				// bail early if no value
+				if( empty($value2) ) continue;
+
+				// find the position of $post_id within $value2 so we can remove it
+				$pos = array_search($post_id, $value2);
+
+				// remove
+				unset( $value2[ $pos] );
+
+				// update the un-selected post's value (use field's key for performance)
+				update_field($field_key, $value2, $post_id2);
+
+			}
+
+		}
+
+		// reset global varibale to allow this filter to function as per normal
+		$GLOBALS[ $global_name ] = 0;
+
+		// return
+		return $value;
+
+	}
+
+
+	/**
 	 * Handle the API calls
 	 */
 	public static function api_call() {
@@ -287,6 +374,14 @@ class Jobsity {
 		$api_data = json_decode( $data );
 		error_log( print_r( $api_data, true ) );
 
+	}
+
+
+	/**
+	 * Retrieves extra data for a single movie
+	 */
+	public static function get_movie_extra_data( $api_movie_id ) {
+		error_log( 'Getting the extra information' );
 	}
 
 }
