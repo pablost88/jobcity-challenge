b7aed69b0f9a776efc96dd14fb85ccb580b13f21
Pablo ST <pablost88@gmail.com>
2022-09-24 12:28:35 -0300

    homepage logic

--- wp-content/themes/moviesandactors/functions.php
+++ wp-content/themes/moviesandactors/functions.php
@@ -13,19 +13,58 @@ register_nav_menus(
 	)
 );
 
+add_theme_support( 'post-thumbnails' );
+
 
 /**
- * Get all movies from the database
+ * Get upcoming movies from the database
+ *
+ * @return $movies_query Movies with a release date greater than the actual date
  */
-function get_movies() {
-	$query = new WP_Query(
+function get_upcoming_movies() {
+
+	// Find current date time.
+	$date_now = date( 'Y-m-d H:i:s' );
+
+	$movies_query = new WP_Query(
 		array(
-			'posts_per_page' => 10,
+			'posts_per_page' => -1,
 			'post_type'      => 'movie',
 			'post_status'    => 'publish',
+			'meta_query'     => array(
+				array(
+					'key'     => 'release_date',
+					'compare' => '>=',
+					'value'   => $date_now,
+					'type'    => 'DATETIME',
+				),
+			),
+			'meta_key'       => 'release_date',
+			'meta_type'      => 'DATETIME',
+			'orderby'        => 'meta_value',
+			'order'          => 'asc',
 		)
 	);
 
-	error_log( 'The movies for the homepage are: ' );
-	error_log( print_r( $query, true ) );
+	return $movies_query;
+}
+
+
+/**
+ * Get populñar actors from the database.
+ */
+function get_popular_actors() {
+	$actor_query = new WP_Query(
+		array(
+			'posts_per_page' => -1,
+			'post_type'      => 'actor',
+			'post_status'    => 'publish',
+			'meta_key'       => 'popularity',
+			'meta_type'      => 'number',
+			'orderby'        => 'meta_value',
+			'order'          => 'desc',
+		)
+	);
+
+	return $actor_query;
 }
--- wp-content/themes/moviesandactors/index.php
+++ wp-content/themes/moviesandactors/index.php
@@ -13,15 +13,115 @@
  */
 
 get_header();
-echo 'This is the new theme';
-
-$movies = get_movies();
 ?>
 
 <!-- Show Movies -->
-<div>
+<?php
+$movies_query = get_upcoming_movies();
 
-</div>
+if ( $movies_query->have_posts() ) :
+	$movie_year  = -1;
+	$movie_month = -1;
+
+	?>
+
+	<div>
+
+	<?php
+	while ( $movies_query->have_posts() ) :
+		$movies_query->the_post();
+
+		$release_date        = get_field( 'release_date', false, false );
+		$time_input          = strtotime( $release_date );
+		$date_input          = getDate( $time_input );
+		$release_date_format = date( 'Y-m-d', $date_input[0] );
+
+		$movie_terms = get_the_terms( $post->ID, 'genre' );
+
+		/*
+		error_log( get_the_title() );
+		error_log( $release_date );
+		error_log( $release_date_format );
+		error_log( print_r( $date_input, true ) );
+		error_log( 'Terms' );
+		error_log( print_r( $terms, true ) );
+		*/
+
+		if ( $movie_year !== $date_input['year'] ) {
+			if ( -1 === $movie_year ) {
+				echo '<div>';
+			} else {
+				echo '</div></div><div>';
+			}
+
+			$movie_year = $date_input['year'];
+			echo '<h1>Movies from ' . esc_attr( $movie_year ) . '</h1>';
+			$movie_month = -1;
+		};
+
+		if ( $movie_month !== $date_input['mon'] ) {
+			if ( -1 === $movie_month ) {
+				echo '<div>';
+			} else {
+				echo '</div><div>';
+			}
+
+			$movie_month = $date_input['mon'];
+
+			// Create date object to store the DateTime format.
+			$month_obj = DateTime::createFromFormat( '!m', $movie_month );
+
+			// Store the month name to variable.
+			$movie_month_name = $month_obj->format( 'F' );
+			echo '<h2>' . esc_attr( $movie_month_name ) . '</h2>';
+		}
+
+		?>
+
+		<div>
+			<h2><?php the_title(); ?></h2>
+			<h4><?php echo esc_attr( $release_date_format ); ?></h4>
+			<ul>
+			<?php
+			foreach ( $movie_terms as $movie_term ) {
+				echo '<li>' . esc_attr( $movie_term->name ) . '</li>';
+			}
+			?>
+			</ul>
+		</div>
+
+	<?php endwhile; ?>
+
+	</div>
+
+<?php endif; ?>
+
+<?php
+	wp_reset_postdata();
+	$actors_query = get_popular_actors();
+
+	//error_log( 'Popular actors' );
+	//error_log( print_r( $actor_query, true ) );
+
+if ( $actors_query->have_posts() ) :
+	echo '<div>';
+	while ( $actors_query->have_posts() ) :
+		$actors_query->the_post();
+		$actor_name = get_field( 'actor_name' );
+		?>
+
+		<div>
+			<h2><?php echo esc_attr( $actor_name ); ?></h2>
+		</div>
+
+		<?php
+	endwhile;
+	echo '</div>';
+	endif;
+
+	wp_reset_postdata();
+?>
+
 
 
 <!-- Show Actors -->
