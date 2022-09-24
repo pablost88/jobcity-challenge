0b611b83f195ac4e4e21bd8028a20ac8d18965f4
Pablo ST <pablost88@gmail.com>
2022-09-24 15:05:15 -0300

    add archive pages

---
+++ wp-content/themes/moviesandactors/archive-actor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The archive template file for actors
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package moviesandactors
+ */
+
+get_header();
+?>
+
+<?php
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		echo the_title();
+	endwhile;
+endif;
+?>
+
+
+<?php
+get_footer();
